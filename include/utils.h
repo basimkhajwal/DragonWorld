6d7f3efbb912cd88abbe690bec35f9086c5b5ce8
Basim Khajwal <basimkhajwal@gmail.com>
2016-07-01 17:51:33 +0100

    Removed temp file

--- include/utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/*
- * Load the specified shaders into the GL context
- * and return the ID created for them
- * */
-GLuint LoadShaders(const char* vertexFilePath, const char* fragmentFilePath);
