a485560fcb986381649b0b2e336e4dc3fbb15fea
Basim Khajwal <basimkhajwal@gmail.com>
2016-07-11 14:30:28 +0100

    Made some more changes

--- src/systems/GridRenderSystem.cpp
+++ src/systems/GridRenderSystem.cpp
@@ -5,18 +5,18 @@ GridRenderSystem::GridRenderSystem(int width, int height, const char* grid, Mesh
     this->wallMesh = wallMesh;
     this->matrixID = matrixID;
 
-    vector<mat4> rotationMatrices(4);
-    rotationMatrices[0] = mat4();
-    rotationMatrices[1] = rotate(mat4(), pi<float>()/2.0f, vec3(0, 1, 0));
-    rotationMatrices[2] = translate(mat4(), vec3(0, 0, -1));
-    rotationMatrices[3] = translate(mat4(), vec3(1, 0, 0)) * rotationMatrices[1];
+    vector<mat4> cubeTransforms(4);
+    cubeTransforms[0] = mat4();
+    cubeTransforms[1] = rotate(mat4(), pi<float>()/2.0f, vec3(0, 1, 0));
+    cubeTransforms[2] = translate(mat4(), vec3(0, 0, -1));
+    cubeTransforms[3] = translate(mat4(), vec3(1, 0, 0)) * cubeTransforms[1];
 
     for (int j = 0; j < height; j++) {
         for (int i = 0; i < width; i++) {
             if (grid[j*width+i] != '1') continue;
 
             mat4 transformMatrix = translate(mat4(), vec3(i, 0, -j));
-            for (mat4& m: rotationMatrices) modelMatrices.push_back(transformMatrix * m);
+            for (mat4& m: cubeTransforms) modelMatrices.push_back(transformMatrix * m);
         }
     }
 }
